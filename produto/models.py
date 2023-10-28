21a7d57798bd80b706ad4245c3ad57360c028a56
jeffersoncardoso100 <tatianibonecher@gmail.com>
2023-10-28 20:08:32 -0300

    web

--- produto/models.py
+++ produto/models.py
@@ -7,8 +7,7 @@ class Contato(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     mensagem = models.TextField()
-    data_envio = models.DateTimeField()  # Remova auto_now_add=True removido
-    
+    data_envio = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.nome
